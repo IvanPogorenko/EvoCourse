53325b882810c13c2b8243d79c37791861bce92d
ivan_pogorenko <ipogorenko@yandex.ru>
2025-04-10 23:42:21 +0700

    Form

--- EvoApp/src/app/app.component.ts
+++ EvoApp/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IBook} from "./interfaces/IBook";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'EvoApp';
+export class AppComponent{
+
+  public books: IBook[] = [
+    {
+      id: 1,
+      title: "Преступление и наказание",
+      author: "Достоевский"
+    }
+  ]
+
+  public addedBook: IBook = {
+    id: 0,
+    title: "",
+    author: ""
+  }
+
+  public onSubmit(){
+    const lastBook = this.books.at(-1)
+    const id = lastBook ? lastBook.id + 1 : 1
+    this.books.push({
+      id: id,
+      title: this.addedBook.title,
+      author: this.addedBook.author
+    })
+  }
+
 }
